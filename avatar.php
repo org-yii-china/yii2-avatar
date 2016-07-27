b16d0cc7ee6b967f9606bbaae03fec2ace1ec909
huangxianan <huangxianan@jndl.mobi>
2016-07-27 16:44:31 +0800

    创建项目

---
+++ avatar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace hyii2\avatar;
+
+/**
+ * This is just an example.
+ */
+class avatar extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
